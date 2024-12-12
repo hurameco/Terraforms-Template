f07e8e7df5fc4a673d5549aa4488a750b5a13b1b
Hugo Coelho <43422673+hurameco@users.noreply.github.com>
2024-12-12 10:30:43 +0000

    Created RDS Instance

---
+++ aws_rds_cluster_teste1.tf
@@ -0,0 +1,33 @@
+
+      resource "aws_rds_cluster" "teste1" {
+        
+	allow_major_version_upgrade: false,
+	apply_immediately: false,
+	backup_retention_period: 1,
+	copy_tags_to_snapshot: false,
+	delete_automated_backups: true,
+	deletion_protection: true,
+	engine_mode: "provisioned",
+	engine_lifecyce_support: "open-source-rds-extended-support",
+	iam_roles: [],
+	performance_insights_retention_period: "7",
+	scaling_configuration: {
+		auto_pause: true,
+		max_capacity: 16,
+		min_capacity: 1,
+		seconds_before_timeout: 300,
+		seconds_until_auto_pause: 300,
+		timeout_action: "RollbackCapacityChange"
+	},
+	skip_final_snapshot: false,
+	storage_encryption: false,
+	storage_type: "",
+	tags: [],
+	vpc_security_group_ids: [],
+	allocated_space: 50,
+	db_cluster_instance_class: "db.m5.8xlarge",
+	engine: "postgres",
+	master_username: "admin",
+	master_password: "password"
+}
+      }
