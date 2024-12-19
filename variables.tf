a29bb478cdd40ed3244d486003cd8f368697af48
Hugo Coelho <43422673+hurameco@users.noreply.github.com>
2024-12-19 15:00:59 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,25 @@ variable "ami" {
   description = "The AMI to use for the instance"
   type        = string
 }
+
+variable "number" {
+  description = "test field for a number"
+  type        = number
+  default     = 10
+}
+
+variable "bool" {
+  description = "test field for a boolean"
+  type        = bool
+  default     = true
+}
+
+variable "empty_number" {
+  description = "test field for a number"
+  type        = number
+}
+
+variable "empty_bool" {
+  description = "test field for a boolean"
+  type        = bool
+}
